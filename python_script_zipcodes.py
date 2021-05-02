d741e8b8c7ddbbe3a11e1268737e9cfeebc53069
Hants <hantsawilliams@gmail.com>
2021-05-01 20:44:19 -0400

    initiat commit and file

---
+++ python_script_zipcodes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  1 20:40:58 2021
+
+@author: hantswilliams + kojodanish 
+
+Stonybrook 
+
+Zipcode Verification Script 
+
+
+1) Register unique ID from USPS: https://registration.shippingapis.com/ 
+
+2) Webtool homepage documentation: https://www.usps.com/business/web-tools-apis/documentation-updates.htm
+
+3) Address information: https://www.usps.com/business/web-tools-apis/address-information-api.pdf
+
+
+"""
+
+
+
+
+import requests 
+import pandas as pd 
+
